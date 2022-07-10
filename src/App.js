2a4230ec65929e35163b6641765c3856ee19286a
kartikmax <kyadav5213@gmail.com>
2022-07-10 18:58:33 +0530

    countries and continents are coming from queries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { gql, useQuery } from "@apollo/client";
+
+const continent = gql`
+  query continent {
+    continents {
+      code
+      name
+    }
+  }
+`;
+
+const allCountries = gql`
+  query findmyCountry($code: ID!) {
+    continent(code: $code) {
+      name
+      countries {
+        name
+        code
+      }
+    }
+  }
+`;
+const App = () => {
+  const { data, loading } = useQuery(continent);
+
+  const [continents2, setContinents2] = useState();
+  const [code, setCode] = useState("AS");
+  const [countriesData, setCountriesData] = useState();
+
+  const { data: theirCountry, loading: loadingCountries } = useQuery(
+    allCountries,
+    {
+      variables: {
+        code: code,
+      },
+    }
   );
-}
+
+  useEffect(() => {
+    if (data) {
+      setContinents2(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (theirCountry) {
+      setCountriesData(theirCountry);
+    }
+  }, [theirCountry]);
+
+  // console.log(countriesData);
+
+  const handleChange = (event) => {
+    setCode(event.target.value);
+    console.log(code, "state");
+  };
+
+  return (
+    <>
+      hello world
+      <br />
+      {loading ? (
+        <h1 style={{ color: "red" }}>loading...</h1>
+      ) : (
+        <>
+          {continents2 &&
+            continents2.continents.map((x) => (
+              <span>
+                <button key={x.code} onClick={handleChange} value={x.code}>
+                  {x.name}
+                </button>
+                <br />
+              </span>
+            ))}
+        </>
+      )}
+      <h1>countries</h1>
+      {loadingCountries ? (
+        <>loading</>
+      ) : (
+        <ol>
+          {countriesData &&
+            countriesData.continent.countries.map((x) => (
+              <button key={x.code}>{x.name}</button>
+            ))}
+        </ol>
+      )}
+    </>
+  );
+};
 
 export default App;
